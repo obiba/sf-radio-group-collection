kazoompa/sf-yes-no-option

Initial commit.

gulpfile.js
@@ -1,0 +1,52 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var clean = require('gulp-clean');
+var templateCache = require('gulp-angular-templatecache');
+var streamqueue = require('streamqueue');
+var fs = require('fs');
+
+gulp.task('default', ['minify']);
+gulp.task('run', ['minify', 'connect', 'watch']);
+
+gulp.task('connect', function () {
+  connect.server({
+    root: ['demo', './'],
+    port: 8888,
+    livereload: true,
+  });
+});
+
+gulp.task('reload', ['minify'], function () {
+  gulp.src('./dist/**/*.*').pipe(connect.reload());
+});
+
+gulp.task('clean', function () {
+  return gulp.src('./dist', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./src/**', './demo/**'], ['reload']);
+});
+
+gulp.task('minify', function () {
+  var files = JSON.parse(fs.readFileSync('sources.json', 'utf-8'));
+  var stream = streamqueue({ objectMode: true },
+    gulp.src(['src/templates/**/*.html']).pipe(templateCache({
+      standalone: true,
+      root: 'src/templates/',
+      module: 'sfYesNoOptionTemplates'
+    })),
+    gulp.src(files)
+  )
+  .pipe(concat('sf-yes-no-option.js'))
+  .pipe(gulp.dest('./dist'))
+  .pipe(uglify())
+  .pipe(rename('sf-yes-no-option.min.js'))
+  .pipe(gulp.dest('./dist'));
+
+  return stream;
+});

obiba/sf-radio-group-collection

gulpfile.js
@@ -1,0 +1,52 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var clean = require('gulp-clean');
+var templateCache = require('gulp-angular-templatecache');
+var streamqueue = require('streamqueue');
+var fs = require('fs');
+
+gulp.task('default', ['minify']);
+gulp.task('run', ['minify', 'connect', 'watch']);
+
+gulp.task('connect', function () {
+  connect.server({
+    root: ['demo', './'],
+    port: 8888,
+    livereload: true,
+  });
+});
+
+gulp.task('reload', ['minify'], function () {
+  gulp.src('./dist/**/*.*').pipe(connect.reload());
+});
+
+gulp.task('clean', function () {
+  return gulp.src('./dist', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./src/**', './demo/**'], ['reload']);
+});
+
+gulp.task('minify', function () {
+  var files = JSON.parse(fs.readFileSync('sources.json', 'utf-8'));
+  var stream = streamqueue({ objectMode: true },
+    gulp.src(['src/templates/**/*.html']).pipe(templateCache({
+      standalone: true,
+      root: 'src/templates/',
+      module: 'sfRadioGroupCollectionTemplates'
+    })),
+    gulp.src(files)
+  )
+  .pipe(concat('sf-radio-group-collection.js'))
+  .pipe(gulp.dest('./dist'))
+  .pipe(uglify())
+  .pipe(rename('sf-radio-group-collection.min.js'))
+  .pipe(gulp.dest('./dist'));
+
+  return stream;
+});